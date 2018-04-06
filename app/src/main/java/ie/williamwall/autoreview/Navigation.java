490f1ab5eb6c91d30a42ad21694d9854f945f7b4
william-wall <william@williamwall.ie>
2018-04-06 17:07:01 +0100

    Fragment integration to Navigation Drawer

--- app/src/main/java/ie/williamwall/autoreview/Navigation.java
+++ app/src/main/java/ie/williamwall/autoreview/Navigation.java
@@ -1,5 +1,9 @@
 package ie.williamwall.autoreview;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +21,10 @@ public class Navigation extends AppCompatActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -27,5 +33,41 @@ public class Navigation extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectItemDrawer(MenuItem menuItem){
+        android.support.v4.app.Fragment myFragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.loc:
+                fragmentClass = Location.class;
+                break;
+            case R.id.wea:
+                fragmentClass = Weather.class;
+                break;
+            default:
+                fragmentClass = Location.class;
+        }
+        try{
+            myFragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent,myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
     }
 }
